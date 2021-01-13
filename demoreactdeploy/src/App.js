7509c20b890ec7b0e9de9a41c6cb9613ca6a4b71
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2021-01-13 13:42:26 +0500

    Update App.js

--- demoreactdeploy/src/App.js
+++ demoreactdeploy/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to react deployment 
-        </a>
-      </header>
+      <h1>This is react app deploynment</h1>
     </div>
   );
 }
