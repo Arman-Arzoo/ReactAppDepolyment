f6ade615f0b8e4b34ca1dcd92a79f583a694aabc
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2021-01-13 14:09:18 +0500

    Update App.js

--- demoreactdeploy/src/App.js
+++ demoreactdeploy/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is react app deploynment</h1>
+      <h1>This is react app deploynment surge</h1>
     </div>
   );
 }
