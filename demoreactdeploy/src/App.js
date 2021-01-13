d91a203fb49e8a21154568fb2560134b3c343236
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2021-01-13 13:34:54 +0500

    Update App.js

--- demoreactdeploy/src/App.js
+++ demoreactdeploy/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to react deployment 
         </a>
       </header>
     </div>
